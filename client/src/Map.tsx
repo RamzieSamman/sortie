ccd4d32cfd6a5f8cc547ad3a3d383a990093af4a
Ramzie Samman <ramziesamman02@gmail.com>
2024-03-21 06:51:54 -0400

    added cursor selector at the top left, needs to be made functional

--- client/src/Map.tsx
+++ client/src/Map.tsx
@@ -5,7 +5,10 @@ import { graphTimer, mapHandler, Spawn, SpawnAsset, spawnHandler} from './Auxili
 import PhysicsObj from './physicsObj/PhysicsObj.tsx'
 import { launchMissile } from './physicsObj/Interaction.tsx'
 import throttle from 'lodash.throttle'
-import { dragToScroll } from './mapInteractions/MapInteraction.tsx'
+import { dragToScroll, dragToSelect } from './mapInteractions/MapInteraction.tsx'
+import cursorPointer from './assets/cursor-pointer.svg'
+import cursorGrab from './assets/cursor-grab.svg'
+
 
 interface ContextProvider {
   masterWidth: number,
@@ -45,79 +48,19 @@ export function Map() { // 480p resolution
     setBegin( (begin) => !begin)
   }
 
-  const dragToSelect = (e) => {
-    // remove previous selection box if it exist
-    document.getElementById('selection-box')?.remove()
-    document.getElementById('qausi-box')?.remove()
-
-    // get the starting click poisition
-    let startX = e.pageX
-    let startY = e.pageY
-    let drag = true
-
-    // create the selection div
-    let box = document.createElement("div")
-    box.setAttribute('id', 'selection-box')
-
-
-    let qausiBox = document.createElement("div")
-    qausiBox.setAttribute('id', 'qausi-box')
-    qausiBox.style.cssText = "z-index: 20; top: 0px; left: 0px; position: absolute; width: 100%; height: 100%;"
-    map_y.current?.appendChild(qausiBox)
-
-    const startDrag = (e) => {
-        if (!drag) return
-        // prevent selection of text or objects
-        e.preventDefault()
-
-        // get the transversal position
-        const x = e.pageX
-        const y = e.pageY
-        console.log(x)
-        const walkX = x - startX
-        const walkY = y - startY
-        const negativeWalkX = -walkX
-        const negativeWalkY = -walkY 
-
-        // if walkX is positive, else spawn the div differently
-        if (walkX >= 0 && walkY >= 0) {
-          box.style.cssText = "z-index: 20; top: "+startY+"px; left: "+startX+"px; position: absolute; width: "+walkX+"px; height: "+walkY+"px; border-style: dotted; border-width: 2px; border-color: rgb(0 0 0)"
-        } else if (walkX < 0 && walkY > 0) {
-          box.style.cssText = "z-index: 20; top: "+startY+"px; left: "+x+"px; position: absolute; width: "+negativeWalkX+"px; height: "+walkY+"px; border-style: dotted; border-width: 2px; border-color: rgb(0 0 0)"
-        } else if (walkX >= 0 && walkY < 0) {
-          box.style.cssText = "z-index: 20; top: "+y+"px; left: "+startX+"px; position: absolute; width: "+walkX+"px; height: "+negativeWalkY+"px; border-style: dotted; border-width: 2px; border-color: rgb(0 0 0)"
-        } else if (walkX < 0 && walkY < 0) {
-          box.style.cssText = "z-index: 20; top: "+y+"px; left: "+x+"px; position: absolute; width: "+negativeWalkX+"px; height: "+negativeWalkY+"px; border-style: dotted; border-width: 2px; border-color: rgb(0 0 0)"
-        }
-
-        // add the selection div to the map
-        map_y.current?.appendChild(box)
-    }
-    map_y.current?.addEventListener('mousemove', startDrag)
-
-    map_y.current?.addEventListener('mouseup', (e) => {
-        drag = false
-    })
-
-    //map_y.current?.addEventListener('mouseleave', (e) => {
-    //    drag = false
-    //})
-
-  }
-
   // spawn rockets
   launchMissile(setSpawns, spawns[0], masterWidth, masterHeight) 
 
   return (
     <Context.Provider value={{masterWidth, masterHeight, graphTime, begin, spawns, setSpawns}}>
-        <div className="relative z-0 cursor-grab">
+        <div className="relative z-0 cursor-pointer">
           <div
             style={{
                 backgroundSize: '10px 10px', backgroundImage: 'linear-gradient(to right, #d9d9d9 1px, transparent 1px), linear-gradient(to bottom, #d9d9d9 1px, transparent 1px)',
                 width: '15000px', height: '15000px'
               }}
             ref={map_y}
-            onMouseDown={(e) => dragToSelect(e)}
+            onMouseDown={(e) => dragToSelect(e, map_y)}
           >
           </div>
               {spawns.map( (spawn, index) => (
@@ -128,6 +71,10 @@ export function Map() { // 480p resolution
                   indexSpawn={index}
               />
               ))}
+            <div className="fixed z-10 select-none bg-white border-2 border-slate-300 rounded-md p-2 h-10 mt-3 flex flex-row" style={{left: '20px', top: '0px'}}>
+                <div className='px-2'> <img src={cursorPointer} width="20px" /> </div>
+                <div className='px-2'> <img src={cursorGrab} width="20px" /> </div>
+            </div>
         </div>
         <button className="bg-indigo-500 m-4 p-4" onClick={toggleBegin}>begin</button>
     </Context.Provider>
